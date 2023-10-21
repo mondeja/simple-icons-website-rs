bb2e5fd4d6b3f6563cd458dff020abdc7a4b391f
Álvaro Mondéjar <mondejar1994@gmail.com>
2023-10-21 20:26:40 +0200

    Fix page padding style error (#86)

--- app/src/pages.rs
+++ app/src/pages.rs
@@ -49,7 +49,7 @@ pub fn Index() -> impl IntoView {
 #[component]
 pub fn Preview() -> impl IntoView {
     view! {
-        <menu class="-mt-4 lg:bg-transparent flex flex-row lg:flex-col">
+        <menu class="page-padding-x -mt-4 lg:bg-transparent flex flex-row lg:flex-col">
             <ColorSchemeControl/>
             <div class=concat!(
                 "flex items-center relative left-3 lg:-left-0.5",
@@ -64,17 +64,19 @@ pub fn Preview() -> impl IntoView {
 
             </div>
         </menu>
-        <PreviewGenerator/>
+        <div class="page-padding-x">
+            <PreviewGenerator/>
+        </div>
     }
 }
 
 #[component]
 pub fn Error404() -> impl IntoView {
     view! {
-        <menu class="-mt-4 bg-transparent">
+        <menu class="page-padding-x -mt-4 bg-transparent">
             <ColorSchemeControl/>
         </menu>
-        <div class="-mt-2 sm:-mt-[52px] flex flex-col items-center justify-center h-full">
+        <div class="page-padding-x -mt-2 sm:-mt-[52px] flex flex-col items-center justify-center h-full">
             <h1 class="text-8xl font-bold">{"404"}</h1>
             <p class="text-2xl font-bold">{move_tr!("page-not-found")}</p>
             <hr class="w-1/2 my-4 border-t-[var(--divider-color)]"/>
