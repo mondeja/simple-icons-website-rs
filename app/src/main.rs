9da2718fb49d09a2391fa272f2703ffbf4dac54d
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-04-30 14:57:26 +0200

    Show loader while loading the app (#227)

--- app/src/main.rs
+++ app/src/main.rs
@@ -11,11 +11,16 @@ pub(crate) mod head;
 pub(crate) mod pages;
 
 use crate::app::App;
-use leptos::{mount_to_body, view};
+use leptos::{document, mount_to_body, view};
 use log::Level;
 
 pub fn main() {
     _ = console_log::init_with_level(Level::Debug);
     console_error_panic_hook::set_once();
+
+    // Remove #loader and body background
+    document().get_element_by_id("loader").unwrap().remove();
+    document().body().unwrap().remove_attribute("style").ok();
+
     mount_to_body(|| view! { <App/> });
 }
