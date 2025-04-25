e35c3d836fe661908320edc12df350addd7b2734
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2025-04-25 11:04:54 +0200

    Simplify CSS minification using PostCSS (#339)

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -2,9 +2,11 @@
  * @file PostCSS configuration file.
  */
 
+import process from 'node:process';
 import autoprefixer from 'autoprefixer';
 import postcssFontsourceUrl from 'postcss-fontsource-url';
 import postcssImport from 'postcss-import';
+import postcssLightningcss from 'postcss-lightningcss';
 import tailwindcss from 'tailwindcss';
 
 /** @type {import('postcss-load-config').Config} */
@@ -14,6 +16,9 @@ const config = {
 		tailwindcss,
 		autoprefixer,
 		postcssFontsourceUrl({directory: '/assets/fonts'}),
+		postcssLightningcss({
+			lightningcssOptions: {minify: process.env.NODE_ENV === 'production'},
+		}),
 	],
 };
 
