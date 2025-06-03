9246505014495551bebb3dfde1c855a35df726e3
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2025-06-03 03:36:47 +0200

    Simplify i18n configuration (#369)

--- app/i18n/src/lib.rs
+++ app/i18n/src/lib.rs
@@ -1,23 +1,6 @@
 use leptos::prelude::*;
 use leptos_fluent::leptos_fluent;
 
-fluent_templates::static_loader! {
-    static TRANSLATIONS = {
-        locales: "./locales",
-        fallback_language: "en-US",
-        // Whithout configuring no isolation, a lot of character marks are
-        // added to the text and makes the website impossible to test.
-        //
-        // According to fluent-templates documentation, these marks are used
-        // to display the text correctly in right-to-left languages. We don't
-        // currently have any right-to-left languages in the project, so it's
-        // disabled to make testing easier. We can enable it later if this
-        // affects the rendering of right-to-left languages.
-        // See https://github.com/XAMPPRocky/fluent-templates?tab=readme-ov-file#why-is-there-extra-characters-around-the-values-of-arguments
-        customise: |bundle| bundle.set_use_isolating(false),
-    };
-}
-
 #[component]
 pub fn I18n(
     children: Children,
@@ -27,7 +10,6 @@ pub fn I18n(
     leptos_fluent! {
         children: children(),
         locales: "./locales",
-        translations: [TRANSLATIONS],
         default_language: "en-US",
         check_translations: "../../{app,components}/**/*.rs",
         sync_html_tag_lang: true,
